e346f689896309442b5201c46982cf8468f515f3
lachlanarboit <lachlan.arboit@my.jcu.edu.au>
2021-04-19 23:47:26 +1000

    Add classes

---
+++ prac_06/guitar.py
@@ -0,0 +1,30 @@
+"""
+CP1404 Solution
+Guitar class
+"""
+CURRENT_YEAR = 2017
+VINTAGE_AGE = 50
+
+class Guitar:
+    """Guitar class for storing details of a guitar"""
+    def _init_(self, name="", year=0, cost=0):
+        """Initialise a guitar"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def _str_(self):
+        """Return a string representation of a guitar"""
+        return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        """Get the age of a guitar based on the CURRENT_YEAR"""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Determine if a guitar is considered vintage or not based on age"""
+        return self.get_age() >= VINTAGE_AGE
+
+    def _lt_(self, other):
+        """Less than, used for storing Guitars - by year released."""
+        return self.year < other.year
