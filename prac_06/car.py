24df6e323d76afb885ea528608f901067025e6dc
lachlanarboit <lachlan.arboit@my.jcu.edu.au>
2021-04-19 16:46:32 +1000

    Complete starter code as required

--- prac_06/car.py
+++ prac_06/car.py
@@ -1,16 +1,26 @@
-"""CP1404/CP5632 Practical - Car class example."""
+"""
+CP1404 Practical
+Car class
+"""
 
 
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+        name = string,reference name for car
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self.odometer = 0
+
+    def _str_(self):
+        """"Return string representation of car object"""
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)
+
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
