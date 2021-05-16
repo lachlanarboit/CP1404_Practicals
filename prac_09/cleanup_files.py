d8ee38f246ec0ccd5874ac44a7d0f4742c844592
lachlanarboit <lachlan.arboit@my.jcu.edu.au>
2021-05-16 23:07:42 +1000

    Add get_fixed_filename function

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -22,5 +22,9 @@ def main():
             os.rename(full_name, new_name)
 
 def get_fixed_filename(filename):
+    """Return a fixed version of filename"""
+    new_name = filename.replace("","_").replace(".TXT", ".txt")
+    return new_name
+
 
 main()
